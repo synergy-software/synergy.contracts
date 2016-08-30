c76782201709c7b4e9a4bbb74abe79fbd1e17e2c
marcin.celej <marcin.celej>
2016-08-30 06:30:17 +0200

    Comment Fail classes #7 - added comment to DesignByContractViolationException

--- Synergy.Contracts/Failures/DesignByContractViolationException.cs
+++ Synergy.Contracts/Failures/DesignByContractViolationException.cs
@@ -5,48 +5,30 @@ using JetBrains.Annotations;
 namespace Synergy.Contracts
 {
     /// <summary>
-    /// Wyj¹tek rzucany g³ównie przez klasê <see cref="Fail"/>. Mówi on o tym, ¿e œwiat zewnêtrzny
-    /// dla naszej logiki nie spe³ni³ kontraktu jaki nasz kod zak³ada³ - np. przyszed³ null do metody
-    /// a my nie zak³adaliœmy, ¿e mo¿e przyjœæ. Nasz kod zak³ada³, ¿e jest uruchomiony na serwerze a 
-    /// okaza³o siê inaczej, itd. <br/>
-    /// 
-    /// Ten exception nigdy nie powinien byæ rzucany. Jeœli go widzisz 
-    /// oznacza to, ¿e jest coœ nie tak ze œwiatem zewnêtrznym, który nie spe³nia kontraktu danej metody.
+    /// The exception thrown when some contract check failed.
+    /// When you see it it means that someone does not meet the contract.
     /// </summary>
     [Serializable]
     public class DesignByContractViolationException : Exception
     {
         /// <summary>
-        /// Wyj¹tek rzucany g³ównie przez klasê <see cref="Fail"/>. Mówi on o tym, ¿e œwiat zewnêtrzny
-        /// dla naszej logiki nie spe³ni³ kontraktu jaki nasz kod zak³ada³ - np. przyszed³ null do metody
-        /// a my nie zak³adaliœmy, ¿e mo¿e przyjœæ. Nasz kod zak³ada³, ¿e jest uruchomiony na serwerze a 
-        /// okaza³o siê inaczej, itd. <br/>
-        /// 
-        /// Ten exception nigdy nie powinien byæ rzucany. Jeœli go widzisz 
-        /// oznacza to, ¿e jest coœ nie tak ze œwiatem zewnêtrznym, który nie spe³nia kontraktu danej metody.
+        /// Constructs the exception with no message.
         /// </summary>
         public DesignByContractViolationException()
         {
         }
 
         /// <summary>
-        /// Wyj¹tek rzucany g³ównie przez klasê <see cref="Fail"/>. Mówi on o tym, ¿e œwiat zewnêtrzny
-        /// dla naszej logiki nie spe³ni³ kontraktu jaki nasz kod zak³ada³ - np. przyszed³ null do metody
-        /// a my nie zak³adaliœmy, ¿e mo¿e przyjœæ. Nasz kod zak³ada³, ¿e jest uruchomiony na serwerze a 
-        /// okaza³o siê inaczej, itd. <br/>
-        /// 
-        /// Ten exception nigdy nie powinien byæ rzucany. Jeœli go widzisz 
-        /// oznacza to, ¿e jest coœ nie tak ze œwiatem zewnêtrznym, który nie spe³nia kontraktu danej metody.
+        /// Constructs the exception with a message.
         /// </summary>
+        /// <param name="message"></param>
         public DesignByContractViolationException([NotNull] string message) : base(message)
         {
         }
 
         /// <summary>
-        /// Konstruktor s³u¿¹cy do deserializacji. Bez niego ten wyj¹tek nie jest w stanie sie zdeserializowaæ.
+        /// Serialization required constructor.
         /// </summary>
-        /// <param name="info"></param>
-        /// <param name="context"></param>
         protected DesignByContractViolationException(SerializationInfo info, StreamingContext context) :
             base(info, context)
         {
