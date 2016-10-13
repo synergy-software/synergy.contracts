aa72ca7af5e1cafb209b051723c28678fddc8943
marcin.celej <marcin.celej@gmail.com>
2016-10-13 14:31:26 +0200

    Prepare JetBrains.Annotations samples #16 - added sample of [SourceTemplate]

---
+++ Synergy.Contracts.Samples/Annotations/SourceTemplateAttributeSample.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace Synergy.Contracts.Samples.Annotations
+{
+    public static class SourceTemplateAttributeSample
+    {
+        [SourceTemplate]
+        public static void forEach<T>(this IEnumerable<T> xs)
+        {
+            foreach (T x in xs)
+            {
+                //$ $END$
+            }
+        }
+
+        [SourceTemplate]
+        public static void newGuid(this object obj, [Macro(Expression = "guid()", Editable = -1)] string newguid)
+        {
+            Console.WriteLine("$newguid$");
+        }
+
+        private static void test()
+        {
+            var enumerable = new List<string>();
+
+            //enumerable.forEach
+            //enumerable.newGuid
+
+            string s = null;
+            //s.fi
+        }
+    }
+}
