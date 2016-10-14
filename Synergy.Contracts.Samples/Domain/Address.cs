0955911cd2c2b4c08a671f6873ed38c0a2f23d9d
marcin.celej <marcin.celej@gmail.com>
2016-10-14 14:18:24 +0200

    Added missing file

---
+++ Synergy.Contracts.Samples/Domain/Address.cs
@@ -0,0 +1,10 @@
+﻿using JetBrains.Annotations;
+
+namespace Synergy.Contracts.Samples.Domain
+{
+    public class Address
+    {
+        [NotNull]
+        public string City { get; set; }
+    }
+}
