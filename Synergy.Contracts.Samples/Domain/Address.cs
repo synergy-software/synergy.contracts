1e9a7b5d4e37f089e2f75a7cf4eab1cce53b830d
marcin.celej <marcin.celej@gmail.com>
2016-10-23 13:21:34 +0200

    Improved R# settings

--- Synergy.Contracts.Samples/Domain/Address.cs
+++ Synergy.Contracts.Samples/Domain/Address.cs
@@ -4,6 +4,11 @@ namespace Synergy.Contracts.Samples.Domain
 {
     public class Address
     {
+        public Address([NotNull] string city)
+        {
+            this.City = city;
+        }
+
         [NotNull]
         public string City { get; set; }
     }
