1abab885ea46ff46960e4b9289a20951b7547b00
marcin.celej <marcin.celej@gmail.com>
2016-11-06 15:55:40 +0100

    Changed Pool and string extensions - so they are reused in other Synergy projects

--- Synergy.Contracts/Extensions/StringFormatExtensions.cs
+++ Synergy.Contracts/Extensions/StringFormatExtensions.cs
@@ -1,8 +1,15 @@
-﻿using System;
+﻿// ------------------------------------------------------------------------
+//
+// WARN: This file is reused between Synergy projects - change it carefully
+// 
+// ------------------------------------------------------------------------
+
+using System;
 using JetBrains.Annotations;
 using Synergy.Pooling;
 
 // ReSharper disable once CheckNamespace
+
 namespace Synergy.Extensions
 {
     /// <summary>
@@ -13,34 +20,39 @@ namespace Synergy.Extensions
 #else
     public
 #endif
-    static class StringExtensions
+            static class StringFormatExtensions
     {
         private static readonly Action<object[]> clearArray = array => Array.Clear(array, 0, array.Length);
 
         private static readonly Pool<object[]> poolOf1ElementArrays = new Pool<object[]>(
             constructor: () => new object[1],
             initialSize: 200,
-            destructor: StringExtensions.clearArray);
+            destructor: StringFormatExtensions.clearArray);
 
         private static readonly Pool<object[]> poolOf2ElementArrays = new Pool<object[]>(
             constructor: () => new object[2],
             initialSize: 100,
-            destructor: StringExtensions.clearArray);
+            destructor: StringFormatExtensions.clearArray);
 
         private static readonly Pool<object[]> poolOf3ElementArrays = new Pool<object[]>(
             constructor: () => new object[3],
             initialSize: 50,
-            destructor: StringExtensions.clearArray);
+            destructor: StringFormatExtensions.clearArray);
+
+        private static readonly Pool<object[]> poolOf4ElementArrays = new Pool<object[]>(
+            constructor: () => new object[4],
+            initialSize: 25,
+            destructor: StringFormatExtensions.clearArray);
 
         /// <summary>
         /// Formats the specified string using {0} annottations.
         /// </summary>
         [NotNull]
         public static string Format(
-            [NotNull] this string format, 
+            [NotNull] this string format,
             [CanBeNull] object arg1)
         {
-            using (Pooled<object[]> args = StringExtensions.poolOf1ElementArrays.Get())
+            using (Pooled<object[]> args = StringFormatExtensions.poolOf1ElementArrays.Get())
             {
                 args.Value[0] = arg1;
                 return string.Format(format, args.Value);
@@ -52,11 +64,11 @@ namespace Synergy.Extensions
         /// </summary>
         [NotNull]
         public static string Format(
-            [NotNull] this string format, 
-            [CanBeNull] object arg1, 
+            [NotNull] this string format,
+            [CanBeNull] object arg1,
             [CanBeNull] object arg2)
         {
-            using (Pooled<object[]> args = StringExtensions.poolOf2ElementArrays.Get())
+            using (Pooled<object[]> args = StringFormatExtensions.poolOf2ElementArrays.Get())
             {
                 args.Value[0] = arg1;
                 args.Value[1] = arg2;
@@ -69,12 +81,12 @@ namespace Synergy.Extensions
         /// </summary>
         [NotNull]
         public static string Format(
-            [NotNull] this string format, 
-            [CanBeNull] object arg1, 
+            [NotNull] this string format,
+            [CanBeNull] object arg1,
             [CanBeNull] object arg2,
             [CanBeNull] object arg3)
         {
-            using (Pooled<object[]> args = StringExtensions.poolOf3ElementArrays.Get())
+            using (Pooled<object[]> args = StringFormatExtensions.poolOf3ElementArrays.Get())
             {
                 args.Value[0] = arg1;
                 args.Value[1] = arg2;
@@ -84,11 +96,32 @@ namespace Synergy.Extensions
         }
 
         /// <summary>
-        /// Formats the specified string using {0}, {1}, {2}, ... annottations.
+        /// Formats the specified string using {0}, {1}, {2}, {3} annottations.
         /// </summary>
         [NotNull]
         public static string Format(
-            [NotNull] this string format, 
+            [NotNull] this string format,
+            [CanBeNull] object arg1,
+            [CanBeNull] object arg2,
+            [CanBeNull] object arg3,
+            [CanBeNull] object arg4)
+        {
+            using (Pooled<object[]> args = StringFormatExtensions.poolOf4ElementArrays.Get())
+            {
+                args.Value[0] = arg1;
+                args.Value[1] = arg2;
+                args.Value[2] = arg3;
+                args.Value[3] = arg4;
+                return string.Format(format, args.Value);
+            }
+        }
+
+        /// <summary>
+        /// Formats the specified string using {0}, {1}, {2}, {3}, ... annottations.
+        /// </summary>
+        [NotNull]
+        public static string Format(
+            [NotNull] this string format,
             [NotNull] params object[] args)
         {
             return string.Format(format, args);
--- Synergy.Contracts/Pooling/Pool.cs
+++ Synergy.Contracts/Pooling/Pool.cs
@@ -1,8 +1,16 @@
-﻿using System;
+﻿// ------------------------------------------------------------------------
+//
+// WARN: This file is reused between Synergy projects - change it carefully
+// 
+// ------------------------------------------------------------------------
+
+using System;
 using System.Collections.Generic;
 using JetBrains.Annotations;
+using Synergy.Contracts;
 
 // ReSharper disable once CheckNamespace
+
 namespace Synergy.Pooling
 {
     /// <summary>
@@ -14,7 +22,7 @@ namespace Synergy.Pooling
 #else
     public
 #endif
-    class Pool<TPooled>
+        class Pool<TPooled>
     {
         [NotNull]
         internal readonly Func<TPooled> Constructor;
@@ -22,14 +30,19 @@ namespace Synergy.Pooling
         [CanBeNull]
         internal readonly Action<TPooled> Destructor;
 
-        [NotNull] 
+        [NotNull]
         private readonly Stack<Pooled<TPooled>> items;
 
-        [NotNull] 
+        [NotNull]
         private readonly object syncRoot = new object();
 
+        /// <summary>
+        /// Creates a pool of objects.
+        /// </summary>
         public Pool([NotNull] Func<TPooled> constructor, int initialSize = 1, [CanBeNull] Action<TPooled> destructor = null)
         {
+            Fail.IfArgumentNull(constructor, nameof(constructor));
+
             this.Constructor = constructor;
             this.Destructor = destructor;
             this.items = new Stack<Pooled<TPooled>>(initialSize);
@@ -43,6 +56,7 @@ namespace Synergy.Pooling
         /// <summary>
         /// Gets the object from the pool.
         /// </summary>
+        [NotNull]
         public Pooled<TPooled> Get()
         {
             lock (this.syncRoot)
@@ -57,8 +71,10 @@ namespace Synergy.Pooling
         /// <summary>
         /// Returns the object to the pool.
         /// </summary>
-        public void Free(Pooled<TPooled> pooled)
+        public void Free([NotNull] Pooled<TPooled> pooled)
         {
+            Fail.IfArgumentNull(pooled, nameof(pooled));
+
             lock (this.syncRoot)
             {
                 this.items.Push(pooled);
@@ -66,20 +82,36 @@ namespace Synergy.Pooling
         }
     }
 
+    /// <summary>
+    /// Pool object container. It contains the pooled object in <see cref="Value"/> property.
+    /// When the object is disposed it returns to the pool it originates from.
+    /// </summary>
 #if INTERNAL_POOL
     internal
 #else
     public
 #endif
-    class Pooled<TPooled> : IDisposable
+        class Pooled<TPooled> : IDisposable
     {
-        public TPooled Value { get; }
+        [NotNull] 
         private readonly Pool<TPooled> pool;
 
+        /// <summary>
+        /// Gets the pooled object.
+        /// </summary>
+        [NotNull]
+        public TPooled Value { get; }
+
+        /// <summary>
+        /// Creates a pooled object from the specified pool.
+        /// </summary>
         public Pooled([NotNull] Pool<TPooled> pool)
         {
-            this.Value = pool.Constructor();
+            Fail.IfArgumentNull(pool, nameof(pool));
+
             this.pool = pool;
+            this.Value = pool.Constructor()
+                             .OrFail(nameof(pool.Constructor));
         }
 
         /// <inheritdoc />
